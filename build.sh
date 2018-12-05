1d67b424e85f61cd1bec40d7328ea80f021a3c16
Kihong Heo <kheo@cis.upenn.edu>
2018-12-05 06:09:59 +0000

    [Build] use OPAM 2.0

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -e
 NCPU="$(getconf _NPROCESSORS_ONLN 2>/dev/null || echo 1)"
-OCAML_VERSION="4.06.1+flambda"
+OCAML_VERSION="4.07.1+flambda"
 SPARROW_OPAM_SWITCH=sparrow-"$OCAML_VERSION"
 opam init --compiler=$OCAML_VERSION -j $NCPU --no-setup --yes
-opam switch set -j $NCPU $SPARROW_OPAM_SWITCH --alias-of $OCAML_VERSION
+opam switch create $SPARROW_OPAM_SWITCH $OCAML_VERSION
 eval $(SHELL=bash opam config env --switch=$SPARROW_OPAM_SWITCH)
-opam install depext
+opam install depext --yes
 opam pin add cil https://github.com/KihongHeo/cil.git -n --yes
 opam pin add sparrow . -n --yes
 opam depext apron
