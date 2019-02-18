0d20ab47e56da940411573a60c4f2566bc0da742
Kihong Heo <kheo@cis.upenn.edu>
2019-02-18 12:47:35 -0500

    [Build] fix opam errors

--- build.sh
+++ build.sh
@@ -1,10 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 NCPU="$(getconf _NPROCESSORS_ONLN 2>/dev/null || echo 1)"
 OCAML_VERSION="4.07.1+flambda"
 SPARROW_OPAM_SWITCH=sparrow-"$OCAML_VERSION"
 opam init --compiler=$OCAML_VERSION -j $NCPU --no-setup --yes
-opam switch create $SPARROW_OPAM_SWITCH $OCAML_VERSION
+
+switch_exists=no
+for installed_switch in $(opam switch list --short); do
+  if [[ "$installed_switch" == "$SPARROW_OPAM_SWITCH" ]]; then
+    switch_exists=yes
+    break
+  fi
+done
+if [ "$switch_exists" = "no" ]; then
+  opam switch create $SPARROW_OPAM_SWITCH $OCAML_VERSION
+fi
+
 eval $(SHELL=bash opam config env --switch=$SPARROW_OPAM_SWITCH)
 opam install depext --yes
 opam pin add cil https://github.com/KihongHeo/cil.git -n --yes
