f8a986bedc8d576b5bc01a95b98456ccb789cc1f
Kihong Heo <kihong.heo@kaist.ac.kr>
2020-02-25 16:42:15 +0900

    [CI] optimize CI script

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -e
+
+export OPAMYES=1
+
 NCPU="$(getconf _NPROCESSORS_ONLN 2>/dev/null || echo 1)"
 OCAML_VERSION="4.08.0+flambda"
 SPARROW_OPAM_SWITCH=sparrow-"$OCAML_VERSION"
-opam init --compiler=$OCAML_VERSION -j $NCPU --no-setup --yes
+opam init --compiler=$OCAML_VERSION -j $NCPU --no-setup
 
 switch_exists=no
 for installed_switch in $(opam switch list --short); do
@@ -19,11 +22,11 @@ else
 fi
 
 eval $(SHELL=bash opam config env --switch=$SPARROW_OPAM_SWITCH)
-opam install depext --yes
-opam pin add cil https://github.com/KihongHeo/cil.git -n --yes
-opam pin add sparrow . -n --yes
-opam depext apron
-opam install -j $NCPU sparrow --yes --deps-only
+opam install depext
+opam depext -i apron clangml
+opam pin add cil https://github.com/KihongHeo/cil.git -n
+opam pin add sparrow . -n
+opam install -j $NCPU sparrow --deps-only
 opam pin remove sparrow
 ./configure --enable-tests
 make
