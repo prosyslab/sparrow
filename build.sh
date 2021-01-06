a6a2bfdeed6689d540bedb27501dd95964abf93e
Kihong Heo <kihong.heo@kaist.ac.kr>
2021-01-06 20:30:18 +0900

    [Build] bump ocaml version to 4.11

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 export OPAMYES=1
 
 NCPU="$(getconf _NPROCESSORS_ONLN 2>/dev/null || echo 1)"
-OCAML_VERSION="4.08.0+flambda"
+OCAML_VERSION="4.11.0+flambda"
 SPARROW_OPAM_SWITCH=sparrow-"$OCAML_VERSION"
 opam init --compiler=$OCAML_VERSION -j $NCPU --no-setup
 
@@ -23,7 +23,8 @@ fi
 
 eval $(SHELL=bash opam config env --switch=$SPARROW_OPAM_SWITCH)
 opam install depext
-opam depext apron clangml
+echo -e "\e[31m[NOTE]\e[0m If you are not a sudo user, press Ctrl+D and skip installing system libraries. Contact the sysadmin, if they are not installed."
+opam depext apron clangml || continue
 opam pin add cil https://github.com/prosyslab/cil.git -n
 opam pin add sparrow . -n
 opam install -j $NCPU sparrow --deps-only
